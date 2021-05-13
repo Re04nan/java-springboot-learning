98b56a40ab0af6f56588fe2b413e938d70bb75f5
Re04nan <renan04_marques@hotmail.com>
2021-05-13 17:21:41 -0300

    hello world with spring boot - java

---
+++ hogwarts/src/main/java/com/hogwarts/wizards/HogwartsApplication.java
@@ -0,0 +1,13 @@
+package com.hogwarts.wizards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsApplication.class, args);
+	}
+
+}
---
+++ hogwarts/src/test/java/com/hogwarts/wizards/HogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogwarts.wizards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
